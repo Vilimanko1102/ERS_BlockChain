d2eb66a40586adacf0efa367673cb042ffcf56b6
v_vilimanovic <62159717+Vilimanko1102@users.noreply.github.com>
2023-12-23 13:34:29 +0100

    Add files via upload
    Kreiranje tri osnovna entiteta: Client, Miner, Block
    Kreiranje dva singletona SmartContract i BlockChain
    Kreiranje pomocne klase Data

---
+++ ERS_BlockChain/ERS_BlockChain/Domain/Entities/ClientEntity.cs
@@ -0,0 +1,28 @@
+﻿using ERS_BlockChain.Domain.Other;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERS_BlockChain.Domain.Entities
+{
+	public class ClientEntity : IEntity<int>
+	{
+		public int ClientID { get; set; }
+		public List<Data> DataToSend { get; set; }
+
+		public ClientEntity() { }
+
+		public ClientEntity(int clientID)
+		{
+			ClientID = clientID;
+			DataToSend = new List<Data>();
+		}
+
+		public int GetId()
+		{
+			return ClientID;
+		}
+	}
+}
